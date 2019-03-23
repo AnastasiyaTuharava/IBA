40755cc6e89779c09b2254d12a1f21d6658f340b
kkurachova <v.cat.mailbox@gmail.com>
2019-03-23 11:36:51 +0300

    Update PositionPattern.java

--- src/by/iba/management/model/entity/PositionPattern.java
+++ src/by/iba/management/model/entity/PositionPattern.java
@@ -9,12 +9,12 @@ import by.iba.management.model.entity.Tools;
  * Created by katya on 3/3/2019.
  */
 public class PositionPattern {
-    Employee qaMiddle = new Employee(false,"junior", "B1", QaTestingPattern.getMidQaTestPattern());
-    Employee qaSenior = new Employee(true, "middle", "B2", QaTestingPattern.getSeniorLeadQaTestPattern());
-    Employee qaLead = new Employee(true, "senior", "C1", QaTestingPattern.getSeniorLeadQaTestPattern());
+    Employee qaMiddle = new Employee(false,"junior", "B1", new Testing(true, false, true, true));
+    Employee qaSenior = new Employee(true, "middle", "B2", new Testing(true, true, true, true));
+    Employee qaLead = new Employee(true, "senior", "C1", new Testing(true, true, true, true));
 
-    Employee devMiddle = new Employee(false, "junior", "B1", ProgrammingLanguage midDevProgrLang_pattern, Skills midDevSkills_pattern, Tools midDevTools_pattern);
-    Employee devSenior = new Employee(true, "middle", "B2", ProgrammingLanguage seniorDevProgrLang_pattern, Skills seniorDevSkills_pattern, Tools seniorLeadDevTools_pattern);
-    Employee devLead = new Employee(true, "senior", "C1", ProgrammingLanguage leadDevProgrLang_pattern, Skills leadDevSkills_pattern, Tools seniorLeadDevTools_pattern);
+    Employee devMiddle = new Employee(false, "junior", "B1", new ProgrammingLanguage(true, true, true, false, false), new Skills(true, true, true, false, false), new Tools(true, true, true, false));
+    Employee devSenior = new Employee(true, "middle", "B2", new ProgrammingLanguage(true, true, true, true, false), new Skills(true, true, true, true, false), new Tools(true, true, true, true);
+    Employee devLead = new Employee(true, "senior", "C1", new ProgrammingLanguage(true, true, true, true, true), new Skills(true, true, true, true, true), new Tools(true, true, true, true);
 
 }
